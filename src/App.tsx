6d71c4394e238e5ae52eb0845c88c5f1311ed481
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-06-03 16:57:36 -0600

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import './App.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Header from './components/Header';
+import { useState } from 'react';
 
 const queryClient = new QueryClient();
 
 function App() {
+  const [search, setSearch] = useState<string>('');
   return (
     <QueryClientProvider client={queryClient}>
-      <div className='container'></div>
+      <div className='container'>
+        <Header setSearch={setSearch} />
+      </div>
     </QueryClientProvider>
   );
 }
