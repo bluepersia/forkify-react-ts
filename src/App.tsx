43332eb476a51c0ba5d3b1a972d0ddc7b1db07f9
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-06-03 16:55:34 -0600

    Search Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import './App.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <div className='container'></div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className='container'></div>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
---
+++ src/components/Header/SearchForm.tsx
@@ -0,0 +1,43 @@
+import {
+  ChangeEvent,
+  Dispatch,
+  FormEvent,
+  SetStateAction,
+  useState,
+} from 'react';
+
+export default function SearchForm({
+  setSearch,
+}: {
+  setSearch: Dispatch<SetStateAction<string>>;
+}): JSX.Element {
+  const [name, setName] = useState<string>('');
+
+  function handleInputChange(e: ChangeEvent): void {
+    setName((e.target as HTMLInputElement).value);
+  }
+
+  function handleSubmit(e: FormEvent): void {
+    e.preventDefault();
+
+    setSearch(name);
+  }
+  return (
+    <form className='search' onSubmit={handleSubmit}>
+      <input
+        name='name'
+        type='text'
+        className='search__field'
+        placeholder='Search over 1,000,000 recipes...'
+        value={name}
+        onChange={handleInputChange}
+      />
+      <button className='btn search__btn'>
+        <svg className='search__icon'>
+          <use href='src/img/icons.svg#icon-search'></use>
+        </svg>
+        <span>Search</span>
+      </button>
+    </form>
+  );
+}
